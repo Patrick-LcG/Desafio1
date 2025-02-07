4b8fd74b2fb2398a1c7c70ef1a2ff691a94d19f5
unknown <gaabpatriick1@gmail.com>
2025-02-07 17:52:12 -0300

    att1

--- desafio1.js
+++ desafio1.js
@@ -1,1 +1,10 @@
 //QUESTÃO 1
+let nome = 'Gabriel';
+console.log(nome);
+
+//QUESTÃO 2
+let idade = '25'
+let altura = '1.75'
+console.log("Tenho " +idade+ " anos e " +altura+ " de altura.")
+
+//QUESTÃO 3
